45b247bf358025a01fbfcc7595f69b6d4176670c
Dhivya Kumar <dhivyadkumar@gmail.com>
2025-11-04 00:40:32 -0500

    added trie implementation using the trie file created earlier. this uses an autocomplete function for the earthquake location names.

--- QuakeWatchWeb/backend/server.py
+++ QuakeWatchWeb/backend/server.py
@@ -3,6 +3,7 @@ from flask_cors import CORS
 from bridges.bridges import *
 from bridges.data_src_dependent import *
 from splay import SplayTree
+from trie import Trie
 
 app = Flask(__name__)
 CORS(app)
@@ -13,11 +14,11 @@ def get_earthquakes():
         bridges = Bridges(0, "pranathim", "1735501070239")
         data = get_earthquake_usgs_data(1000)
 
-        tree = SplayTree()
+        splay_tree = SplayTree()
+        trie_tree = Trie()
 
         for quake in data:
-            key = quake.magnitude
-            value = {
+            quake_info = {
                 "title": quake.title,
                 "magnitude": quake.magnitude,
                 "location": quake.location,
@@ -25,14 +26,22 @@ def get_earthquakes():
                 "long": quake.longit,
                 "url": quake.url,
             }
-            tree.insert(key, value)
 
-        result = tree.inorder()    
+            splay_tree.insert(quake.magnitude, quake_info)
+            trie_tree.insert(quake.title, quake_info)
+
+        #result = tree.inorder()    
+        result = {
+            "splay_sorted": splay_tree.inorder(),
+            "total_stored_in_trie": trie_tree.wordCtr
+        }
         
         return jsonify(result)
     except Exception as e:
         return jsonify({"error": str(e)}), 500
     
+
+
 if __name__ == "__main__":
     app.run(debug=True)
 
