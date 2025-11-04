2c058ba4406aed358005764f24c439fe6b4e6468
alyssagriesman <agriesman@icloud.com>
2025-11-03 20:53:30 -0500

    edited our UI by updating our index.css file

--- QuakeWatchWeb/src/index.tsx
+++ QuakeWatchWeb/src/index.tsx
@@ -28,27 +28,19 @@ export default function Home() {
   }, []);
 
   return (
-    <div>
+    <div id = "homepage">
       <h1>Earthquake Map</h1>
       <div style={{ display: 'flex', gap: '20px', marginTop: '20px', width: '1000px' }}>
         <div style={{ flex: 3 }}>
           <EarthquakeMap earthquakes={earthquakes} center={mapCenter} />
         </div>
-        <div style ={{ 
-          flex: 1,
-          backgroundColor: 'white',
-          border: '1px solid #ccc',
-          borderRadius: '10px',
-          color:'black', 
-          textAlign: 'left', 
-          padding: '10px',
-          }}>
+        <div id = "sidebar" style ={{ flex: 1}}>
           <h3>Location: </h3>
-            <p>{location}</p>
+            <p className = 'answers'>{location}</p>
           <h3>Magnitude: </h3>
-            <p>{magnitude}</p>
+            <p className = 'answers'>{magnitude}</p>
             <h3>More Info:</h3>
-            <p style={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
+            <p className = 'answers' style={{ wordBreak: 'break-word', overflowWrap: 'break-word' }}>
               <a href={url} target="_blank" rel="noreferrer">{url}</a>
             </p>        
          </div>
